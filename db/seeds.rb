84ac52b48254710a788c17fa147d85c5ce44b5d9
Mubeen Mughal <mubeenmughal111@gmail.com>
2023-03-06 19:50:22 +0500

    add Seedme file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+# Create PersonalData records
+employment = Employment.create(employer: "Google", date_started: Date.today, date_ended: Date.tomorrow)
+
+# Create Employment records and associate with PersonalData
+personal_data1 = PersonalData.create(first_name: "John", last_name: "Doe", nickname: "Johnny", email: "john.doe@example.com", phone: "555-555-5555", employment_id: employment.id)
+personal_data2 = PersonalData.create(first_name: "Jane", last_name: "Smith", nickname: "Janie", email: "jane.smith@example.com", phone: "555-123-4567", employment_id: employment.id)
